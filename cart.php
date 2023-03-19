7c5bbdedfe02bf57a63f2668eec0321bacc46608
Shalom Mageza <97250002+magezashalom1@users.noreply.github.com>
2023-03-19 12:22:21 -0700

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,8 @@
+<div id="cart">
+  <h2>Cart</h2>
+  <ul id="cart-items">
+    <!-- Cart items will be added dynamically using JavaScript -->
+  </ul>
+  <p>Total: $<span id="cart-total">0</span></p>
+  <button id="checkout-button">Checkout</button>
+</div>
